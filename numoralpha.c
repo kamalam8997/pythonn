970354795415e643f1df027390489344dd299255
kamalam8997 <shanmughapriya1997@gmail.com>
2018-03-27 16:09:02 +0530

    Create numoralpha.c

---
+++ numoralpha.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the value",num);
+    scanf("%d",&num);
+    if((num=0)||(num=1)||(num=2)||(num=3)||(num=4)||(num=5)||(num=6)||(num=7)||(num=8)||(num=9))
+     printf("yes");
+     else
+     printf("no");
+    return 0;
+}
